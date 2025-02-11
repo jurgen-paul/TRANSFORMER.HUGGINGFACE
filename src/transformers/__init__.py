0af4650079baa82f727663f93956772c1e7ba91c
Matt <rocketknight1@gmail.com>
2025-02-11 15:54:14 +0000

    More init cleanup

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -697,9 +697,7 @@ _import_structure = {
     "models.persimmon": ["PersimmonConfig"],
     "models.phi": ["PhiConfig"],
     "models.phi3": ["Phi3Config"],
-    "models.phi3_5": [
-        "Phi3VConfig",
-    ],
+    "models.phi3_5": ["Phi3VConfig"],
     "models.phimoe": ["PhimoeConfig"],
     "models.phobert": ["PhobertTokenizer"],
     "models.pix2struct": [
@@ -5889,9 +5887,7 @@ if TYPE_CHECKING:
     )
     from .models.phi import PhiConfig
     from .models.phi3 import Phi3Config
-    from .models.phi3_5 import (
-        Phi3VConfig,
-    )
+    from .models.phi3_5 import Phi3VConfig
     from .models.phimoe import PhimoeConfig
     from .models.phobert import PhobertTokenizer
     from .models.pix2struct import (
