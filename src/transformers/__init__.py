73f3f3ac0a758870b3d7814a069cbca7fc15c1b4
Alex-Brooks <Alex.brooks@ibm.com>
2025-03-24 19:17:54 +0000

    Code formatting

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -5679,13 +5679,13 @@ if TYPE_CHECKING:
     )
     from .models.gptj import GPTJConfig
     from .models.granite import GraniteConfig
-    from .models.granitemoe import GraniteMoeConfig
-    from .models.granitemoeshared import GraniteMoeSharedConfig
     from .models.granite_speech import (
+        GraniteSpeechConfig,
         GraniteSpeechEncoderConfig,
         GraniteSpeechProjectorConfig,
-        GraniteSpeechConfig,
     )
+    from .models.granitemoe import GraniteMoeConfig
+    from .models.granitemoeshared import GraniteMoeSharedConfig
     from .models.grounding_dino import (
         GroundingDinoConfig,
         GroundingDinoProcessor,
@@ -7576,6 +7576,12 @@ if TYPE_CHECKING:
             GraniteModel,
             GranitePreTrainedModel,
         )
+        from .models.granite_speech import (
+            GraniteSpeechEncoderProjectorPreTrainedModel,
+            GraniteSpeechForConditionalGeneration,
+            GraniteSpeechPreTrainedModel,
+            GraniteSpeechQFormerModel,
+        )
         from .models.granitemoe import (
             GraniteMoeForCausalLM,
             GraniteMoeModel,
@@ -7585,12 +7591,6 @@ if TYPE_CHECKING:
             GraniteMoeSharedForCausalLM,
             GraniteMoeSharedModel,
             GraniteMoeSharedPreTrainedModel,
-        )
-        from .models.granite_speech import (
-            GraniteSpeechForConditionalGeneration,
-            GraniteSpeechPreTrainedModel,
-            GraniteSpeechEncoderProjectorPreTrainedModel,
-            GraniteSpeechQFormerModel,
         )
         from .models.grounding_dino import (
             GroundingDinoForObjectDetection,
@@ -9387,8 +9387,8 @@ if TYPE_CHECKING:
     except OptionalDependencyNotAvailable:
         from .utils.dummy_torchaudio_objects import *
     else:
-        from .models.musicgen_melody import MusicgenMelodyFeatureExtractor, MusicgenMelodyProcessor
         from .models.granite_speech import GraniteSpeechFeatureExtractor, GraniteSpeechProcessor
+        from .models.musicgen_melody import MusicgenMelodyFeatureExtractor, MusicgenMelodyProcessor
     try:
         if not is_flax_available():
             raise OptionalDependencyNotAvailable()
